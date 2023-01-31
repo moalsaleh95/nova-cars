4f31bdd112e6a345d4fff2e1ccf3624f318cd155
Mohammad Al Saleh <mo.alsaleh95@gmail.com>
2023-01-31 17:54:42 +0300

    NOVA-16

--- src/components/Faq/Faq.tsx
+++ src/components/Faq/Faq.tsx
@@ -3,12 +3,12 @@ import FaqAccordion from '../FaqAccordion/FaqAccordion';
 
 function Faq() {
   return (
-    <div className='py-20 flex flex-row justify-center bg-[#f9f9f9]'>
-      <div className='basis-2/5'>
-        <p className='text-4xl w-72 font-bold'>Frequently Asked Questions</p>
+    <div className='mx-auto px-10 py-12 flex flex-row flex-wrap justify-center bg-[#f9f9f9]'>
+      <div className='container mx-auto basis-2/6 '>
+        <p className='text-4xl w-72 font-bold py-8'>Frequently Asked Questions</p>
         <p className='w-72 mt-3'>Here you can find the answers to frequently asked questions</p>
       </div>
-      <div className='bsis-3/5'>
+      <div className='basis-4/6 '>
         <FaqAccordion />
       </div>
     </div>
