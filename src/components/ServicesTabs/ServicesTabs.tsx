830f857d96e7d57c8061122cceda135908282bde
MohammedJR <mohammed.j@hotelistan.com>
2023-02-01 14:56:34 +0300

    feat: Nova-14 - icons

--- src/components/ServicesTabs/ServicesTabs.tsx
+++ src/components/ServicesTabs/ServicesTabs.tsx
@@ -1,111 +1,154 @@
 import React, { FC, useState } from 'react'
 import { Tab } from '@headlessui/react'
+import {ReactComponent as  CarWashIcon}  from '../../assets/icons/carWashIcon.svg'
+import {ReactComponent as  carTransportationIcon }  from '../../assets/icons/carTransportationIcon.svg'
+import {ReactComponent as  carMaintenanceIcon }  from '../../assets/icons/carMaintenanceIcon.svg'
+import {ReactComponent as  carPaintingIcon }  from '../../assets/icons/carPaintingIcon.svg'
+import {ReactComponent as  carWashIcon }  from '../../assets/icons/carWashIcon.svg'
+import {ReactComponent as  bodyRepairIcon }  from '../../assets/icons/bodyRepairIcon.svg'
+import {ReactComponent as  expertiseReportIcon }  from '../../assets/icons/expertiseReportIcon.svg'
+
+
+const tabsData = [
+    {
+        id: '1',
+        name: 'Roadside Assistance',
+        Icon: carTransportationIcon,
+        content: 'Roadside Assistance'
+    },
+    {
+        id: '2',
+        name: 'Car Wash',
+        Icon: CarWashIcon,
+        content: 'Car Wash',
+    },
+    {
+        id: '3',
+        name: 'Machanical Maintenance',
+        Icon: carMaintenanceIcon,
+        content: 'Machanical Maintenance',
+    },
+    {
+        id: '4',
+        name: 'Paint Repair',
+        Icon: carPaintingIcon,
+        content: 'Paint Repair',
+    },
+    {
+        id: '5',
+        name: 'Body Repair',
+        Icon: bodyRepairIcon,
+        content: 'Body Repair',
+    },
+    {
+        id: '6',
+        name: 'Expertise Report',
+        Icon: expertiseReportIcon,
+        content: 'Expertise Report',
+    },
+    {
+        id: '7',
+        name: 'Mechanical Maintenance',
+        Icon: carMaintenanceIcon,
+        content: 'Mechanical Maintenance',
+    },
+    {
+        id: '8',
+        name: 'Paint Repair',
+        Icon: carPaintingIcon,
+        content: '',
+    },
+]
+
+
+// let [categories] = useState({
+//     'Roadside assistance': [
+//       {
+//         id: 1,
+//         title: 'Does drinking coffee make you smarter?',
+//         date: '5h ago',
+//         commentCount: 5,
+//         shareCount: 2,
+//       },
+//       {
+//         id: 2,
+//         title: "So you've bought coffee... now what?",
+//         date: '2h ago',
+//         commentCount: 3,
+//         shareCount: 2,
+//       },
+//     ],
+//     Popular: [
+//       {
+//         id: 1,
+//         title: 'Is tech making coffee better or worse?',
+//         date: 'Jan 7',
+//         commentCount: 29,
+//         shareCount: 16,
+//       },
+//       {
+//         id: 2,
+//         title: 'The most innovative things happening in coffee',
+//         date: 'Mar 19',
+//         commentCount: 24,
+//         shareCount: 12,
+//       },
+//     ],
+//     Trending: [
+//       {
+//         id: 1,
+//         title: 'Ask Me Anything: 10 answers to your questions about coffee',
+//         date: '2d ago',
+//         commentCount: 9,
+//         shareCount: 5,
+//       },
+//       {
+//         id: 2,
+//         title: "The worst advice we've ever heard about coffee",
+//         date: '4d ago',
+//         commentCount: 1,
+//         shareCount: 2,
+//       },
+//     ],
+//   })
 
 
 const ServicesTabs: FC = () => {
-    let [categories] = useState({
-        Recent: [
-          {
-            id: 1,
-            title: 'Does drinking coffee make you smarter?',
-            date: '5h ago',
-            commentCount: 5,
-            shareCount: 2,
-          },
-          {
-            id: 2,
-            title: "So you've bought coffee... now what?",
-            date: '2h ago',
-            commentCount: 3,
-            shareCount: 2,
-          },
-        ],
-        Popular: [
-          {
-            id: 1,
-            title: 'Is tech making coffee better or worse?',
-            date: 'Jan 7',
-            commentCount: 29,
-            shareCount: 16,
-          },
-          {
-            id: 2,
-            title: 'The most innovative things happening in coffee',
-            date: 'Mar 19',
-            commentCount: 24,
-            shareCount: 12,
-          },
-        ],
-        Trending: [
-          {
-            id: 1,
-            title: 'Ask Me Anything: 10 answers to your questions about coffee',
-            date: '2d ago',
-            commentCount: 9,
-            shareCount: 5,
-          },
-          {
-            id: 2,
-            title: "The worst advice we've ever heard about coffee",
-            date: '4d ago',
-            commentCount: 1,
-            shareCount: 2,
-          },
-        ],
-      })
     
       return (
         <div className="container w-full py-16 bg-[#f1f1f1] ">
                 <Tab.Group>
                     <Tab.List className="flex w-full space-x-1 rounded-xl bg-[#F1F1F1]  pt-1">
-                    {Object.keys(categories).map((category) => (
+                    {tabsData.map((tab) => {
+                        const {name, Icon, id} = tab
+                        return (
                         <Tab
-                            key={category}
+                            key={id}
                             className={({ selected }) =>
                                 (
-                                ` w-full rounded-t-lg py-2.5 text-sm font-normal leading-5 text-blue-700',
-                                'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
+                                ` w-full flex flex-col justify-start items-center space-y-4  rounded-t-lg py-2.5 text-sm font-normal leading-5 text-blue-700 focus:outline-none
+                               
                                 ${selected
-                                    ? 'bg-white text-[#D81212] h-[calc()]'
+                                    ? 'bg-white text-[#D81212]'
                                     : 'text-[#222222] bg-[#f1f1f1] text-opacity-30 hover:bg-white/[0.12] first-line:' } `
                                 )
                             }
                             >
-                            {category}
+                            <Icon />
+                            <span>
+                                {name}
+                            </span>
                         </Tab>
-                    ))}
+                    )})}
                     </Tab.List>
                     <Tab.Panels className="w-full">
-                    {Object.values(categories).map((posts, idx) => (
+                    {tabsData.map((tab, idx) => (
                         <Tab.Panel
                         key={idx}
                         className={'rounded-b-xl bg-white p-3 ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2'}>
-                        <ul>
-                            {posts.map((post) => (
-                            <li
-                                key={post.id}
-                                className="relative rounded-md p-3 hover:bg-gray-100"
-                            >
-                                <h3 className="text-sm font-medium leading-5">
-                                {post.title}
-                                </h3>
-            
-                                <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
-                                <li>{post.date}</li>
-                                <li>&middot;</li>
-                                <li>{post.commentCount} comments</li>
-                                <li>&middot;</li>
-                                <li>{post.shareCount} shares</li>
-                                </ul>
-            
-                                <a
-                                href="#"
-                                className={
-                                    'absolute inset-0 rounded-md ring-blue-400 focus:z-10 focus:outline-none focus:ring-2'}
-                                />
-                            </li>
-                            ))}
-                        </ul>
+                       
+                           {tab.name}
+                        
                         </Tab.Panel>
                     ))}
                     </Tab.Panels>
