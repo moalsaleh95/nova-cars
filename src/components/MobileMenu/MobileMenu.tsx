619ac658c518520a1859cee21bbf19f337f61681
MohammedJR <mohammed.j@hotelistan.com>
2023-02-03 11:25:20 +0300

    feat: Nova-30 - click outside of the menu close the menu

--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -1,16 +1,45 @@
 import { Transition } from '@headlessui/react'
-import React, { FC, useState } from 'react'
+import React, { FC, useEffect, useRef, useState } from 'react'
 import { ReactComponent as Hamburger } from '../../assets/icons/Hamburger.svg'
-
 
 const MobileMenu: FC = () => {
     const [ isMenuOpen, setIsMenuOpen ] = useState<boolean>(false)
+    const drawerRef = useRef<any>()
 
 
     const handleClick = () => {
         setIsMenuOpen(prevState => !prevState)
     }
  
+
+    useEffect(() => {
+        console.log('mobile menu: drawerRef', drawerRef)
+    } ,[drawerRef])
+
+
+      
+  const eventClickOutsideDrawer = (event: MouseEvent) => {
+    if (!drawerRef.current) return;
+
+    // click inside
+    if (drawerRef.current.contains(event.target as Node)) {
+      return;
+    }
+
+    // click outside
+    setIsMenuOpen(false);
+  };
+
+  useEffect(() => {
+    if (eventClickOutsideDrawer) {
+      document.removeEventListener("click", eventClickOutsideDrawer);
+    }
+    document.addEventListener("click", eventClickOutsideDrawer);
+    return () => {
+      document.removeEventListener("click", eventClickOutsideDrawer);
+    };
+  }, []);
+
 
 
   return (
@@ -19,15 +48,17 @@ const MobileMenu: FC = () => {
         <Transition
         show={isMenuOpen}
         enter="transition-opacity transition-[transform_,_opacity] duration-500"
-        enterFrom="opacity-50 -translate-x-[100%]"
+        enterFrom="opacity-50 translate-x-[100%]"
         enterTo="opacity-100 translate-x-0"
         leave="transition-opacity duration-500 transition-[transform_,_opacity]"
         leaveFrom="opacity-100 translate-x-0"
-        leaveTo="opacity-0 -translate-x-[100%]"
-        className='absolute top-0 left-0 h-[calc(100%)] w-2/3 min-w-[250px] min-w- border-2 border-orange-400  z-[100] bg-white'
-        >
-            <div className=''>
+        leaveTo="opacity-0 translate-x-[100%]"
+        className='absolute top-0 right-0 h-[calc(100%)] w-2/3 min-w-[250px] min-w- border-2 border-orange-400  z-[100] bg-white'
+        ref={drawerRef}
 
+        >
+            <div className='w-full h-full border-2 border-pink-600' ref={drawerRef} >               
+                    
             </div>
         </Transition>
         
