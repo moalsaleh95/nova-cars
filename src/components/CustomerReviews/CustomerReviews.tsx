710a589996cbf22a8af4dc5e9e0d9b7b1b0d74b3
MohammedJR <mohammed.j@hotelistan.com>
2023-02-06 12:07:26 +0300

    CustomerRevies for Mobile screens

--- src/components/CustomerReviews/CustomerReviews.tsx
+++ src/components/CustomerReviews/CustomerReviews.tsx
@@ -4,11 +4,11 @@ import { CustomerReviewsCarousel } from '../CustomerReviewsCarousel';
 function CustomerReviews() {
 
   return (
-    <div className='bg-[#D81212] overflow-hidden'>
-        <div className='container poppins flex flex-row justify-center pt-24 bg-[#D81212] h-96'>
+    <div className='bg-[#D81212] overflow-hidden px-5 pb-[61px]'>
+        <div className='container poppins flex flex-col md:flex-row justify-center pt-[72px] md:pt-24 bg-[#D81212] md:h-96'>
             <div className='basis-2/5 text-white '>
-                <p className='text-4xl w-72 font-bold'>Our Customer Reviews</p>
-                <p className='w-72'>From its medieval origins to the digital era, learn everything there is to know about the ubiquitous lorem ipsum passage.</p>
+                <p className='md:text-4xl w-72 text-[24px] font-black md:font-bold'>Our Customer Reviews</p>
+                <p className='md:w-72 mt-4 mb-6'>From its medieval origins to the digital era, learn everything there is to know about the ubiquitous lorem ipsum passage.</p>
             </div>
 
             <div className='basis-3/5 bg-[#D81212] rounded-md'>
--- src/components/CustomerReviewsCarousel/CustomerReviewsCarousel.tsx
+++ src/components/CustomerReviewsCarousel/CustomerReviewsCarousel.tsx
@@ -41,18 +41,31 @@ function CustomerReviewsCarousel() {
             </div>
         )
     })
-
+ 
   return (
     <div className='rounded-t-md overflow-hidden relative'>  
-        <div className='w-[45rem] rounded-md relative overflow-hidden' >   
+        <div className='md:w-[45rem] rounded-md relative overflow-hidden px-0' >   
             <OwlCarousel 
-            navText=
-            {[
-                `<div class='nav-button owl-prev rotate-180'><img class="!w-3" src=${arrow} alt="" /></div>`,
-                `<div class='nav-button owl-next'><img class="!w-3" src=${arrow} alt="" /></div>`
-            ]}
-                mouseDrag= {false} touchDrag={false}
-                stagePadding={0} items={2} margin={0} autoplay ={true} merge={true} nav dots={true} slideBy={2} dotsEach={1} loop>  
+                navText=
+                {[
+                    `<div class='nav-button owl-prev rotate-180'><img class="!w-3" src=${arrow} alt="" /></div>`,
+                    `<div class='nav-button owl-next'><img class="!w-3" src=${arrow} alt="" /></div>`
+                ]}
+                mouseDrag= {false} touchDrag={true}
+                stagePadding={0} margin={0} autoplay ={true} merge={true} nav dots={true} slideBy={2} dotsEach={1} loop={true}
+                responsive= {
+                    {
+                        '1':{
+                            items: 1
+                        },
+                        '768': {
+                            items: 2
+                        }
+                    }
+                    
+                }
+                
+                >  
                 {reviews} 
             </OwlCarousel>  
             <div className='triangle absolute top-[10.5rem]'></div>
