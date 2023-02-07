10b8a42188b82f9b3cedb8b8aefb25a53fd3be42
MohammedJR <mohammed.j@hotelistan.com>
2023-02-07 11:30:54 +0300

    Reviews Slider Loop

--- src/components/CustomerReviewsCarousel/CustomerReviewsCarousel.tsx
+++ src/components/CustomerReviewsCarousel/CustomerReviewsCarousel.tsx
@@ -52,7 +52,7 @@ const CustomerReviewsCarousel = () => {
                     `<div class='nav-button owl-next'><img class="!w-3" src=${arrow} alt="" /></div>`
                 ]}
                 mouseDrag= {false} touchDrag={true}
-                stagePadding={0} margin={0} autoplay ={true} merge={true} nav dots={true} slideBy={2} dotsEach={1} loop={false}
+                stagePadding={0} margin={0} autoplay ={true} merge={true} nav dots={true} slideBy={2} dotsEach={1} loop={true}
                 responsive= {
                     {
                         '1':{
