4f26bcfe79f2dd057d6bdcfb2a54fd3bc624a74b
MohammedJR <mohammed.j@hotelistan.com>
2023-03-07 14:45:35 +0300

    fix: map center

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -8,13 +8,13 @@ interface MapProps {
 
 const Map: FC<MapProps> = ({ wrapperClassNames }) => {
     return (
-        <div className={`${wrapperClassNames} map border border-rose-500 h-[626px]`}>
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{height: '626px'}}>
+        <div className={`${wrapperClassNames} map rounded-[20px] overflow-hidden border border-rose-500 h-[626px]`}>
+            <MapContainer center={[41.062, 28.988]} zoom={15} scrollWheelZoom={false} style={{height: '626px'}}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[51.505, -0.09]}>
+                <Marker position={[41.062, 28.988]}>
                     <Popup>
                         A pretty CSS3 popup. <br /> Easily customizable.
                     </Popup>
