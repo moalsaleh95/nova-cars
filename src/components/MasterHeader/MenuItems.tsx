42187a076049bac7bef52dd0b2f6e875bef7c3fb
MohammedJR <mohammed.j@hotelistan.com>
2023-03-07 18:22:15 +0300

    fix: the navigating to other pages brought back to home page

--- src/components/MasterHeader/MenuItems.tsx
+++ src/components/MasterHeader/MenuItems.tsx
@@ -19,9 +19,10 @@ const MenuItems: FC<MenuItemsProps> = ({
 
   const handleNavLink = (item: NavigationItemType) => {
     // urlNavigate(item.href)
-    if(item.href == '/about') goToPage(item.href);
-    if(item.href == '/contact') goToPage(item.href);
-    else goToPage('/');
+    // if(item.href == '/about') goToPage(item.href);
+    // if(item.href == '/contact') goToPage(item.href);
+    // else goToPage('/');
+    urlNavigate(item.href)
   };
 
   const RenderItem = (item: NavigationItemType) => {
