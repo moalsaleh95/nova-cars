be224f30aae9a6d215b8e85a94ec0d674eff9f75
MohammedJR <mohammed.j@hotelistan.com>
2023-03-07 12:07:34 +0300

    fix: the Page Title bar for contact apge

--- src/views/Contact/Contact.tsx
+++ src/views/Contact/Contact.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ContactInfoCards from './ContactInfoCards'
+import PageTitleBar from './PageTitleBar'
 
 const Contact = () => {
   return (
     <div className='min-h-screen'>
+        <PageTitleBar />
         <ContactInfoCards />
     </div>
   )
