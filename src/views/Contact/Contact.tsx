91ed559e38a01fc18c93f61b6a7dafcadf62ec7b
MohammedJR <mohammed.j@hotelistan.com>
2023-03-07 16:41:54 +0300

    fix: the tablet view of the Contact page

--- src/views/Contact/Contact.tsx
+++ src/views/Contact/Contact.tsx
@@ -9,9 +9,9 @@ const Contact = () => {
     <div className='min-h-screen'>
         <PageTitleBar />
         <ContactInfoCards />
-        <div className='container grid grid-cols-1 xl:grid-cols-3 gap-x-[30px] my-[30px]'>
+        <div className='container grid grid-cols-1 xl:grid-cols-3 gap-x-[30px] lg:mt-4 my-[30px] lg:px-9 2xl:px-0'>
           <Map wrapperClassNames='col-span-2'/>
-          <div className='px-5 lg:px-0 w-full col-span-2 xl:col-span-1 mt-4 lg:mt-0'>
+          <div className='px-5 lg:px-0 w-full col-span-2 xl:col-span-1 mt-[30px] lg:mt-4 2xl:mt-0'>
             <ContactForm />
           </div>
         </div>
