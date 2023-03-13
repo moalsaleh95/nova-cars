e2587d8d066e34346a43021a7ac3cfb1f1f5be22
MohammedJR <mohammed.j@hotelistan.com>
2023-03-13 17:23:58 +0300

    feat: Nova-90 - ContactInfoCard for dark mode

--- src/components/ContactInfoCard/ContactInfoCard.tsx
+++ src/components/ContactInfoCard/ContactInfoCard.tsx
@@ -10,11 +10,11 @@ interface ContactInfoCardProps {
 
 const ContactInfoCard: FC<ContactInfoCardProps> = ({title, subtitle, Icon}) => {
   return (
-    <div className='py-[40px] xl:py-[57px] px-[50px] flex items-center space-x-5 bg-white rounded-[20px]'>
-        <Icon className="w-[30px] lg:w-auto" />
+    <div className='py-[40px] xl:py-[57px] px-[50px] flex items-center space-x-5 bg-white dark:bg-[#0b0b0b] rounded-[20px]'>
+        <Icon className="w-[30px] lg:w-auto dark:text-white" />
         <div className='flex flex-col'>
-            <span className='text-xl lg:text-2xl'>{title}</span>
-            <span className='text-lg opacity-50'>{subtitle}</span>
+            <span className='text-xl lg:text-2xl dark:text-white'>{title}</span>
+            <span className='text-lg opacity-50 dark:text-white'>{subtitle}</span>
         </div>
     </div>
   )
