bb51080e830f39199fcf79b35860d710a7f3459b
MohammedJR <mohammed.j@hotelistan.com>
2023-03-15 18:22:07 +0300

    fix: content for contact page

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -30,15 +30,15 @@ const ContactForm: FC = () => {
   return (
     <div className='w-full xl:w-[450px] rounded-[30px] pt-[37px] pb-[30px] px-[30px] bg-white dark:bg-[#0b0b0b]' style={{"height":"min-content"}}>
         <span className='text-2xl font-normal dark:text-white'>
-            Send a Message
+           Mesaj Yollayın
         </span>
           <form className='w-full space-y-[30px] md:space-y-0 2xl:space-y-[30px] md:gap-4 2xl:gap-0 mt-6 grid grid-cols-1 md:grid-cols-3 xl:grid-cols-1' onSubmit={ handleSubmit } >
-              <AnimatedInput value={values.nameSurname} inputType='text' label='Name, Surname' name='nameSurname' onChange={(e: any) => handleChange(e)}/>
-              <AnimatedInput value={values.phoneNo} inputType='text' label='Phone Number' name='phoneNo' onChange={(e: any) => handleChange(e, 'phoneNo')} />
-              <AnimatedInput value={values.email} inputType='text' label='E-Mail Address' name='email' onChange={(e: any) => handleChange(e)}/>
+              <AnimatedInput value={values.nameSurname} inputType='text' label='İsim, Soyadı' name='nameSurname' onChange={(e: any) => handleChange(e)}/>
+              <AnimatedInput value={values.phoneNo} inputType='text' label='Telefon Numarası' name='phoneNo' onChange={(e: any) => handleChange(e, 'phoneNo')} />
+              <AnimatedInput value={values.email} inputType='text' label='E-Posta Adresi ' name='email' onChange={(e: any) => handleChange(e)}/>
               {/* <AnimatedInput value={values.phoneNo} inputType='text' label='Phone Number' name='phoneNo' onChange={(e: any) => handleChange(e)}/> */}
-              <AnimatedInput value={values.message} inputType='textArea' label='Your Message' name='message' onChange={(e: any) => handleChange(e)} wrapperClassName='h-[150px] md:col-span-3 xl:col-span-1'/>
-              <button className='w-full bg-[#D81212] flex justify-center items-center px-[30px] py-3 lg:py-[18px] text-white text-base rounded-[10px] md:col-span-3 xl:col-span-1'> Send Form </button>
+              <AnimatedInput value={values.message} inputType='textArea' label='Mesajınız' name='message' onChange={(e: any) => handleChange(e)} wrapperClassName='h-[150px] md:col-span-3 xl:col-span-1'/>
+              <button className='w-full bg-[#D81212] flex justify-center items-center px-[30px] py-3 lg:py-[18px] text-white text-base rounded-[10px] md:col-span-3 xl:col-span-1'> Mesajı Yollayınız </button>
           </form>
     </div>
   )
