f4c3928f45de65ba11ff9fe6b386cf3c790a2f3b
MohammedJR <mohammed.j@hotelistan.com>
2023-03-16 09:56:54 +0300

    feat: dynamic routing and data for services

---
+++ src/lib/servicesData.ts
@@ -0,0 +1,70 @@
+const ServicesData = [
+    
+    {
+        id: 'interrior-cleaning',
+        name: 'Detaylı İç Temizlik',
+        content: {
+            title: 'Detaylı İç Temizlik',
+            text: 'Detaylı iç temizlik uygulamasında araç içerisinde temas edilen direksiyon, vites, ayna, kapı kolları, ön gögüs, torpido ve bütün plastik aksam gibi sürücü ve yolcuların sürekli temas ettiği yüzeylerin yanı sıra tavan ve koltuk döşemeleri detaylı bir şekilde anti-bakteriyel temizlik ürünleriyle temizlenerek dezenfekte edilmektedir. Aracınız ilk aldığınız günkü temizliğine ve kokusuna kavuşur.'
+        }
+    },
+    {
+        id:'car-wash',
+        name: 'Oto Yıkama',
+        content: {
+            title: 'Oto Yıkama',
+            text: 'Nova Car Care farkıyla kullanılan yüksek kalitede ki ürünler ve profesyonel hizmet anlayışı ile klasik sulu oto yıkama uygulamalarında bile araçlar çok daha temiz ve detaylı yıkama uygulamasından yararlanmaktadır. Kullanılan oto yıkama köpüğü, özel cilalı şampuan ve hızlı ıslak cilalar ile araçların boya ve kaportalarına en ufak zarar verilmeden özenli bir yıkama hizmeti verilir.' 
+        }
+    },
+    {
+        id:'mechanical-maintenance',
+        name: 'Mekanik Bakım',
+        content: {
+            title: 'Mekanik Bakım',
+            text: 'Mekanik sistemler içerisinde meydana gelebilecek aşınmadan kaynaklı problemleri ortadan kaldırmak ve sistemin sağlığını korumak için düzenli olarak mekanik bakım yapılması gerekmektedir. Mekanik bakım hizmetlerimiz kapsamında otomobilin tüm mekanik aksamları ile ilgili sorunlar tespit edilerek son teknolojiler kullanılarak değişim ve bakımı yapılır.' 
+        }
+    },
+    {
+        id:'paint-repair',
+        name: 'Boya Onarımı',
+        content: {
+            title: 'Boya Onarımı',
+            text: 'Meydana gelen tampon çizikleri, araç boya tamiri hizmeti kapsamında oto mini yama yöntemi ile tamir edilmektedir. Söz konusu kaportada çizik, sıyrık gibi hasarlar tamir edilirken, tamiri yapılacak alan mümkün olduğunca küçük bırakılır. Otomobil tamir boyası rengi orjinal boya rengine özel bir teknik ile uyumlandırılarak hasar tamamen yok edilir.' 
+        }
+    },
+    {
+        id:'body-repair',
+        name: 'Kaporta Onarımı',
+        content: {
+            title: 'Kaporta Onarımı',
+            text: 'Nova Car Care kaporta onarım uygulama hizmeti aracınızın orjinal boyasına zarar vermez. Günümüzdeki araçların bir çoğu metalik boya ile üretildiğinden dolayı kullanmakta olduğumuz yardımcı aparatlar ile mükemmel uygulama tekniklerimiz sayesinde aracınızın kaporta yüzeyine zarar vermeden hasar işlemlerinizi profesyonel bir şekilde gerçekleştiriyoruz.' 
+        }
+    },
+    {
+        id:'polish-paste',
+        name: 'Pasta Cila',
+        content: {
+            title: 'Pasta Cila',
+            text: 'Aracınızın yüzeyinde zamanla çevresel etmenlerle birlikte çizikler, lekeler ve boya matlaşmaları oluşur. Araç yüzeyinde oluşan bu tarz küçük deformasyonlar pasta ve cila işlemleri ile giderilmektedir. Kaliteli ürünler kullanılarak yapılan pasta işleminde öncesinde yüzeydeki çizikler kapatılarak temiz ve pürüzsüz bir yüzey oluşturulur. Ardından yapılan cila işlemiyle ise yüzeyde oluşturulan ince ve şeffaf tabakayla aracınız derin bir parlaklık kazanarak ilk günkü ışıltısına kavuşur.' 
+        }
+    },
+    {
+        id:'paintless-dent-repair',
+        name: 'Boyasız Göçük Onarımı',
+        content: {
+            title: 'Boyasız Göçük Onarımı',
+            text: 'Dış darbeler sonucu araç kaporta aksamı üzerinde meydana gelen, boyanın zarar görmediği kaporta göçüklerinin aracın orjinalliğini bozmadan ve herhangi bir boyama işlemi yapılmaksızın özel aparatlar ile düzeltilerek eski haline getirilme işlemidir. Mini onarım uygulamaları sayesinde çarpma ve park hasarları gibi kaportada meydana gelecek, boyasız düzeltilebilecek tüm hasarlar araç orjinalliği bozulmadan tamir edlmektedir.' 
+        }
+    },
+    {
+        id:'motor-cleaning',
+        name: 'Detaylı Motor Temizlik',
+        content: {
+            title: 'Detaylı Motor Temizlik',
+            text: 'Otomobilin motor kısmında zamanla toz, yağ katmanları, çamur, kireç, zift, demir tozları ve pas birikir. Bu nedenle araç motorunun temizliği çok önemlidir. Motor temizleme işleminde önce çok amaçlı konsantre temizleyiciler ile motor yağı, toz ve kirden arındırılarak temizlenir. Daha sonra kirin nüfuz etmesini zorlaştıran ve motora parlaklık veren özel motor ve plastik aksam koruyucu ile kaplanır.' 
+        }
+    },
+
+]
+
+export default ServicesData
