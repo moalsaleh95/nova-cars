3c756eb94e120abe9afee027498d247f78943ddd
MohammedJR <mohammed.j@hotelistan.com>
2023-03-23 14:51:07 +0300

    feat: the "nasıl giderim" button on the map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -26,7 +26,8 @@ const MarkerIcon = new L.Icon({
 const Map: FC<MapProps> = ({ wrapperClassNames }) => {
     return (
         <div className={`${wrapperClassNames} map  overflow-hidden h-[219px] xl:h-[626px] px-5 lg:px-0`}>
-            <div className='rounded-[20px] overflow-hidden w-full h-full'>
+            <div className='rounded-[20px] overflow-hidden w-full h-full relative'>
+                <a href={`https://www.google.com/maps/dir/Current+Location/41.0628433,28.9841503`} className='absolute right-4 top-4 bg-white bg-opacity-80 border-2 border-[#DDDDDD] px-4 py-2 z-[1005] rounded-[10px] cursor-pointer text-xs' target="_blank">Nasıl Giderim</a>
                 <MapContainer center={[41.118, 29.02]} zoom={15} scrollWheelZoom={false} className='h-full'>
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
