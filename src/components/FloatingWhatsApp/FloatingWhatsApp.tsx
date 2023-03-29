e4b8dafc3c47352e6319d9c419f47477694120e6
Mohammad Al Saleh <mo.alsaleh95@gmail.com>
2023-03-29 15:10:05 +0300

    whatsapp btn color + izle change

--- src/components/FloatingWhatsApp/FloatingWhatsApp.tsx
+++ src/components/FloatingWhatsApp/FloatingWhatsApp.tsx
@@ -279,7 +279,7 @@ export default function FloatingWhatsApp({
         </div>
 
         <a className={`${css.chatFooter} flex`} href='https://wa.me/905549867024' target='_blank'>
-          <button className='font-poppins font-normal !text-white !mx-auto border-[#075e54] !bg-[#075e54] w-full rounded-2xl py-2'>
+          <button className='font-poppins font-normal !text-white !mx-auto border-[#25D366] !bg-[#25D366] w-full rounded-2xl py-2'>
             İletişime Geç
           </button>
         </a>
