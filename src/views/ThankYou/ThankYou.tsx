6a2ca05dbac6fd5b54ca11e0ef8fdef401d45bbd
MohammedJR <mohammed.j@hotelistan.com>
2023-08-08 13:54:32 +0300

    change: the content for the thank you page

--- src/views/ThankYou/ThankYou.tsx
+++ src/views/ThankYou/ThankYou.tsx
@@ -4,9 +4,9 @@ const ThankYou: FC = () => {
     return (
         <section className='h-[80vh]'>
             <div className='container flex flex-col items-center justify-center space-y-8 h-full'>
-                <h1 className='text-3xl '>Thank You!</h1>
+                <h1 className='text-3xl '>Teşekkürler!</h1>
                 <p className='text-lg max-w-[80%] text-center'>
-                    Thank you for getting in touch! We appreciate you contacting us. Our Customer Service Representative will get back to you soon! Have a great day!
+                İletişim kurduğunuz için teşekkür ederiz! Bize ulaştığınızı takdir ediyoruz. Müşteri Hizmetleri Temsilcimiz en kısa sürede size dönüş yapacaktır! Harika bir gün geçirin!
                 </p>
 
             </div>
